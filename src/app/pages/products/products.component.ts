da2a0a195637a904d7b71b7b6df796530ca8a34e
Denys1994q <drybachok@gmail.com>
2024-05-28 15:27:50 +0300

    fix comments

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductsService} from '../../services/products.service';
 import {ProductInterface} from '../../shared/models/product.interface';
-import {Subject, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -11,23 +10,19 @@ import {Subject, takeUntil} from 'rxjs';
 export class ProductsComponent implements OnInit {
   constructor(private productsService: ProductsService) {}
   products!: ProductInterface[];
-  loading: boolean = true;
-  unsubscribe$ = new Subject<void>();
+  loading: boolean = false;
 
   ngOnInit(): void {
-    this.productsService
-      .getProducts()
-      .pipe(takeUntil(this.unsubscribe$))
-      .subscribe({
-        next: (products: ProductInterface[]) => {
-          this.products = products;
-          this.loading = false;
-        }
-      });
+    this.subscribeToProducts();
   }
 
-  ngOnDestroy() {
-    this.unsubscribe$.next();
-    this.unsubscribe$.complete();
+  subscribeToProducts(): void {
+    this.loading = true;
+    this.productsService.getProducts().subscribe({
+      next: (products: ProductInterface[]) => {
+        this.products = products;
+        this.loading = false;
+      }
+    });
   }
 }
--- src/app/pages/products/products.module.ts
+++ src/app/pages/products/products.module.ts
@@ -2,12 +2,11 @@ import {NgModule} from '@angular/core';
 import {ProductsComponent} from './products.component';
 import {RouterModule} from '@angular/router';
 import {SharedModule} from '../../shared/shared.module';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 const routes = [{path: '', component: ProductsComponent}];
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [RouterModule.forChild(routes), SharedModule, MatProgressSpinnerModule]
+  imports: [RouterModule.forChild(routes), SharedModule]
 })
 export class ProductsModule {}
