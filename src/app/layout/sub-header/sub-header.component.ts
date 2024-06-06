d36edf9a6f57c33485f9e6e0839d1a79891db226
Denys1994q <drybachok@gmail.com>
2024-06-06 14:17:46 +0300

    add router link to logo

--- src/app/layout/sub-header/sub-header.component.ts
+++ src/app/layout/sub-header/sub-header.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from '@angular/common';
 import {AuthButtonComponent} from '../../shared/components/buttons/auth-button/auth-button.component';
 import {CartButtonComponent} from '../../shared/components/buttons/cart-button/cart-button.component';
 import {SelectComponent} from '../../shared/components/inputs/select/select.component';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-sub-header',
   standalone: true,
-  imports: [CommonModule, AuthButtonComponent, CartButtonComponent, SelectComponent],
+  imports: [CommonModule, AuthButtonComponent, CartButtonComponent, SelectComponent, RouterModule],
   templateUrl: './sub-header.component.html'
 })
 export class SubHeaderComponent {
