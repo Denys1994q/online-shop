301413052ac46665ec1dec02a25321055adb8114
Denys1994q <drybachok@gmail.com>
2024-06-06 13:24:27 +0300

    fix types

--- src/app/pages/products/services/productDetails.resolver.ts
+++ src/app/pages/products/services/productDetails.resolver.ts
@@ -2,8 +2,11 @@ import {inject} from '@angular/core';
 import {ActivatedRouteSnapshot, ResolveFn} from '@angular/router';
 import {Observable} from 'rxjs';
 import {ProductsService} from '../../../services/products.service';
+import {ProductInterface} from '../../../shared/models/product.interface';
 
-export const ProductDetailsResolver: ResolveFn<any> = (route: ActivatedRouteSnapshot): Observable<any> | null | '' => {
+export const ProductDetailsResolver: ResolveFn<any> = (
+  route: ActivatedRouteSnapshot
+): Observable<ProductInterface> | null | '' => {
   const productId = route.paramMap.get('id');
   const productsService = inject(ProductsService);
   return productId && productsService.getProductById(productId);
