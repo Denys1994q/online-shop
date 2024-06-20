7d3a5e34ec263dbba2ef376bb98ea14746168edd
Denys1994q <drybachok@gmail.com>
2024-06-20 16:19:51 +0300

    fix styles rating

--- src/app/shared/components/mini-product-card/mini-product-card.component.ts
+++ src/app/shared/components/mini-product-card/mini-product-card.component.ts
@@ -1,11 +1,11 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ProductInterface} from '../../models/product.interface';
 import {CartProductInterface} from '../../../pages/shopping-cart/models/cart-product.interface';
 import {ShoppingCartService} from '../../../pages/shopping-cart/services/shopping-cart.service';
 
 @Component({
   selector: 'app-mini-product-card',
-  templateUrl: './mini-product-card.component.html'
+  templateUrl: './mini-product-card.component.html',
+  styleUrl: './mini-product-card.component.sass'
 })
 export class MiniProductCardComponent implements OnInit {
   @Input({required: true}) product!: CartProductInterface;
@@ -19,10 +19,10 @@ export class MiniProductCardComponent implements OnInit {
 
   onAmountInputChange(event: Event): void {
     const newValue = (event.target as HTMLInputElement).value;
-    this.shoppingCartService.updateProductAmount(this.product._id, +newValue)
+    this.shoppingCartService.updateProductAmount(this.product._id, +newValue);
   }
 
   removeFromCart(): void {
-    this.shoppingCartService.removeFromCart(this.product._id)
+    this.shoppingCartService.removeFromCart(this.product._id);
   }
 }
